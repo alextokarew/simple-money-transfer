950bd89e7fda94eca0406d3cd05cfedd0812f3fd
Alexander Tokarev <alex.tokarew@gmail.com>
2018-09-03 07:02:37 +0300

    maxLimit and locked fields for Account class

--- src/main/scala/com/github/alextokarew/moneytransfer/domain/Account.scala
+++ src/main/scala/com/github/alextokarew/moneytransfer/domain/Account.scala
@@ -5,8 +5,10 @@ package com.github.alextokarew.moneytransfer.domain
   * @param id account unique identifier
   * @param description account description
   * @param balance current balance, must be non-negative
+  * @param maxLimit optional maximum balance limit, may be set for some legal purposes
+  * @param locked is this account locked. Locked account doesn't allow any operations
   */
-case class Account(id: AccountId, description: String, balance: BigInt)
+case class Account(id: AccountId, description: String, balance: BigInt, maxLimit: Option[BigInt], locked: Boolean)
 
 /**
   * Value object for representing account id. This object can be serialized to and deserialized from String,
