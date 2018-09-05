021945d2be4c8294ed57d85aa106bf767dddde62
Alexander Tokarev <alex.tokarew@gmail.com>
2018-09-05 10:40:36 +0300

    Removing put method and adding update method to Storage

--- src/main/scala/com/github/alextokarew/moneytransfer/storage/Storage.scala
+++ src/main/scala/com/github/alextokarew/moneytransfer/storage/Storage.scala
@@ -6,16 +6,9 @@ package com.github.alextokarew.moneytransfer.storage
   * @tparam E entity (value) type
   */
 trait Storage[ID, E] {
-  /**
-    * Puts an entity to the storage.
-    * @param id primary key of the entity to put
-    * @param entity an entity to put into the storage
-    * @return the entity
-    */
-  def put(id: ID, entity: E): E
 
   /**
-    * Puts an entity to the storage only if the record with specified id does not exist. Otherwise returns an existing entity
+    * Puts a new entity to the storage. If an entity under the specified key is present returns an existing entity
     * @param id primary key of the entity to put
     * @param entity an entity to put into the storage
     * @return passed entity if the record wasn't create before, or an existing entity
@@ -35,6 +28,14 @@ trait Storage[ID, E] {
     * @return an entity or none if the key was not found
     */
   def get(id: ID): Option[E]
+
+  /**
+    * Updates a value under specified key. If the value does not exist returns None
+    * @param id key to update the value
+    * @param doUpdate update operation that accepts previous value and returns an updated value
+    * @return an updated entity or none if the key was not found
+    */
+  def update(id: ID, doUpdate: E => E): Option[E]
 }
 
 
@@ -47,16 +48,13 @@ class InMemoryStorage[ID, E] extends Storage[ID, E] {
 
   private val map = new java.util.concurrent.ConcurrentHashMap[ID, E]()
 
-  override def put(id: ID, entity: E): E = {
-    map.put(id, entity)
-    entity
-  }
-
   override def putIfAbsent(id: ID, entity: E): E = map.putIfAbsent(id, entity)
 
   override def exists(id: ID): Boolean = map.contains(id)
 
   override def get(id: ID): Option[E] = Option(map.get(id))
+
+  override def update(id: ID, doUpdate: E => E): Option[E] = Option(map.computeIfPresent(id, (_, v) => doUpdate(v)))
 }
 
 object InMemoryStorage {
